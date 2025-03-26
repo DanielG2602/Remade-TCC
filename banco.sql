05f602cb47e486d83964053e5975b2444ba3aa64
Matheus Assis <matheusassis825@gmail.com>
2025-03-26 00:14:27 -0300

    tela de cadastro e entre outras modificações

--- banco.sql
+++ banco.sql
@@ -1,3 +1,24 @@
+- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema AcervoRct
+-- -----------------------------------------------------
+-- Acervos de recetas a serem publicados em forna de livros a serem vendidos no mecado.
+-- 
+
+-- -----------------------------------------------------
+-- Schema AcervoRct
+--
+-- Acervos de recetas a serem publicados em forna de livros a serem vendidos no mecado.
+-- 
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `AcervoRct` ;
 -- -----------------------------------------------------
 -- Schema new_schema1
