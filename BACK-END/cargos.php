2b571603b1b1147e25e3cb927caf4ac4744600f1
MatheusAssis7 <matheusassis825@gmail.com>
2025-05-25 16:24:03 -0300

    Corrige a lógica de inserção no arquivo cargos.php, ajustando os nomes das variáveis e incluindo o campo idCargo na consulta SQL.

--- BACK-END/cargos.php
+++ BACK-END/cargos.php
@@ -3,20 +3,22 @@ include'./conexão.php';
 
 $nomeCargo = $_POST["NomeCargo"];
 $descCargo = $_POST['DescCargo'];
-$dataAdmissao = $_POST['DataAdmissao'];
+$dataAdmissao = $_POST['data_inicio'];
+$dataDemissao = $_POST['data_fim'];
 $status = $_POST['Status'];
 
 try {
     $pdo = conn();
-    $sql = "INSERT INTO cargo ( descricao, data_inicio, data_fim, ind_ativo, nomeCargo) 
+    $sql = "INSERT INTO cargo ( idCargo, descricao, data_inicio, data_fim, ind_ativo, nomeCargo) 
             VALUES (:descricao, :data_inicio, :data_fim, :ind_ativo, :nomeCargo)";
             
     $stmt = $pdo->prepare($sql);
-    $stmt->bindValue('descricao', 'daniel');
-    $stmt->bindValue('data_inicio', '2023-10-01');
-    $stmt->bindValue('data_fim', '2023-10-01');
-    $stmt->bindValue('ind_ativo', true);
-    $stmt->bindValue('nomeCargo', 'daniel');
+    $stmt ->bindValue('idCargo', null);
+    $stmt->bindValue('descricao', $descCargo);
+    $stmt->bindValue('data_inicio', $dataAdmissao);
+    $stmt->bindValue('data_fim', $dataDemissao);
+    $stmt->bindValue('ind_ativo', $status);
+    $stmt->bindValue('nomeCargo', $nomeCargo);
 
     $stmt->execute();
 
