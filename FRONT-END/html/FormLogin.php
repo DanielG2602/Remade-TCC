30d8c41fbbe5fba63f7e0e97120a627d8ef0bd66
MatheusAssis7 <matheusassis825@gmail.com>
2025-05-28 10:30:12 -0300

    Atualiza configuração de conexão com o banco de dados e melhora a estilização das telas de login e cadastro de usuário

--- FRONT-END/html/FormLogin.php
+++ FRONT-END/html/FormLogin.php
@@ -19,29 +19,38 @@ if (isset($_SESSION['usuario_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="css/login.css">
-    </head>
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
 <body>
-    <div id="div_lado_esquerdo"></div>
-    <div id="div_lado_direito">
-        <form action="../BACK-END/ValidaUsuario.php" method="POST">
-            <h1>Preencha os campos abaixo para acessar sua conta.</h1>
+    <main>
+        <div id="lado_esquerdo"></div>  
+        <div id="lado_direito">
+            <form action="../../BACK-END/ValidaUsuario.php" method="post">
+                <h1>Informe os dados de acesso</h1>
+                <p>Preencha os campos abaixo para acessar sua conta.</p>
 
-            <?php if (!empty($erro_login)): ?>
+                <?php if (!empty($erro_login)): ?>
                 <div style="color: red;"><?php echo $erro_login; ?></div>
-            <?php endif; ?>
+                <?php endif; ?>
 
-            <label for="email">Informe seu Email:</label>
-            <input type="email" id="email" name="email" placeholder="Informe seu email" required>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="Informe seu email" required>
 
-            <label for="senha">Informe sua senha:</label>
-            <input type="password" id="senha" name="senha" placeholder="Informe sua senha" required>
+                <label for="senha">Senha:</label>
+                <input type="password" id="senha" name="senha" placeholder="Informe sua senha" required>
 
-            <div class="botoes">
-                <button type="submit">Entrar</button>
-            </div>
-            <p>Não tem uma conta? <a href="FormCadastroUsuario.php">Cadastre-se aqui</a>.</p>
-        </form>
-    </div>
+                <label>
+                    <input type="checkbox" name="manter_conectado"> Manter Conectado
+                </label>
+
+                  <div class="botoes">
+                    <button type="button" class="esqueceu-senha"><a href="../html/FormEsqueceuSenha.html">Esqueceu sua Senha?</a> </button>
+                    <button type="submit" class="conectar">Conecta-se</button>
+                  </div>
+            </form>
+        </div>  
+    </main>
 </body>
-</html>
--- FRONT-END/html/formCadastroUsuario.php
+++ FRONT-END/html/formCadastroUsuario.php
@@ -18,37 +18,44 @@ if (isset($_SESSION['mensagem_registro'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuário</title>
-    <link rel="stylesheet" href="css/cadastro.css"> </head>
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/TelaCadastro.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
 <body>
-    <div id="div_lado_esquerdo"></div>
-    <div id="div_lado_direito">
-        <form action="../BACK-END/RegistroUsuario.php" method="POST">
-            <h1>Registre-se para criar sua conta.</h1>
+    <main>
+        <div id="lado_esquerdo"></div>  
+        <div id="lado_direito">
+            <form action="../../BACK-END/RegistroUsuario.php" method="post">
+                <h1>Registra-se</h1>
+                <p>Será necessario fornecer as seguintes informações:</p>
 
-            <?php if (!empty($mensagem_registro)): ?>
+                <?php if (!empty($mensagem_registro)): ?>
                 <div style="color: <?php echo ($tipo_mensagem === 'sucesso' ? 'green' : 'red'); ?>;">
                     <?php echo $mensagem_registro; ?>
                 </div>
-            <?php endif; ?>
+                <?php endif; ?>
 
-            <label for="email">Informe seu Email:</label>
-            <input type="email" id="email" name="email" placeholder="Informe seu email" required>
+                <label for="email"> Informe seu Email:</label>
+                <input type="email" id="email" name="email" placeholder="Email" required>
 
-            <label for="confirmar_email">Confirme seu Email:</label>
-            <input type="email" id="confirmar_email" name="confirmar_email" placeholder="Confirme seu email" required>
+                <label for="email">Confirme seu Email:</label>
+                <input type="email" id="email" name="email" placeholder="Informe seu email" required>
 
-            <label for="senha">Crie sua Senha:</label>
-            <input type="password" id="senha" name="senha" placeholder="Crie sua senha" required>
 
-            <label for="confirmar_senha">Confirme sua Senha:</label>
-            <input type="password" id="confirmar_senha" name="confirmar_senha" placeholder="Confirme sua senha" required>
+                <label for="senha">Informe sua Senha:</label>
+                <input type="password" id="senha" name="senha" placeholder="senha" required>
 
-            <div class="botoes">
-                <button type="submit">Cadastrar</button>
-            </div>
-            <p>Já tem uma conta? <a href="FormLogin.php">Faça login aqui</a>.</p>
-        </form>
-    </div>
+                <label for="senha">Confirme sua Senha:</label>
+                <input type="password" id="senha" name="senha" placeholder="Informe sua senha" required>        
+
+                <div class="botoes">
+                    <button type="button" class="cancelar"><a href="../html/index.html">Cancelar</a></button>
+                    <button type="submit" class="confirmar">Confirmar</button>
+                </div> 
+            </form>
+        </div>  
+    </main>
 </body>
-</html>
